817b33d72507906186e50926ad63c880ee81cb4c
KM Ramirez <c.d.ramirez1987@gmail.com>
2019-03-22 14:36:36 -0600

    Corrected the syntax for struct initialization and got a refesher course in ownership in Rust (Lines 240, 246, 252)

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 	
 	/*-------------Init--------------*/
 	
-	let mut player = Player	{ Default::default() };
+	let mut player: Player = Default::default();
 	print_line_break();
 	println!("Welcome to RPG!");
 	println!("by KMR");
@@ -15,22 +15,19 @@ fn main() {
 	println!("Please type a name for your Warrior: ");
 	
 	let temp_name = input_name();
-	
 	player.name = temp_name.trim().to_string();
 	
 	print_line_break();
 	
-	//Init player stats
-	let mut stats = Stats { strength: 0, dex: 0, con: 0, int: 0, wis: 0, cha: 0 };
 	//Bool to break loop
 	let mut keep_stats = false;
 	
 	
 	while keep_stats == false {
-		stats = build_stats();
+		player.stats.build();
 		
 		println!("{}'s stats are: ", player.name);
-		print_stats(&stats);
+		player.stats.print();
 		
 		//Asks player if they wish to keep stats, if not, rerolls
 		print_line_break();
@@ -43,7 +40,7 @@ fn main() {
 	}
 	
 	//Converts and applies stat modifiers to player
-	level_up(&mut stats, &mut player);
+	player.level_up();
 	
 	println!("Player stats: {:#?}", player);
 	
@@ -86,6 +83,7 @@ struct Player {
 	gold: i32,
 	xp: i32,
 	level: i16,
+	magic: i16,
 	stats: Stats,
 	inventory: Inventory
 }
@@ -93,7 +91,7 @@ struct Player {
 impl Default for Player {
 	fn default() -> Player {
 		Player { 	
-			name: "Default", 
+			name: "Default".to_string(), 
 			hit_points:		12, 
 			damage: 		0, 
 			armor_class:	0, 
@@ -101,25 +99,41 @@ impl Default for Player {
 			gold: 			10,
 			xp:				0,
 			level:			0,
-			stats: 			Stats { Default::default() },
-			inventory:		Inventory { Default::default() },
-			}
+			magic:			0,
+			stats: 			Stats {	
+								strength: 0,
+								dex: 0,
+								con: 0,
+								int: 0,
+								wis: 0,
+								cha: 0,
+							},
+							
+			inventory:		Inventory {
+								armor: Item { name: "Old Rags".to_string(), effect: 0 },
+								weapon: Item { name: "Bare Hands".to_string(), effect: 0},
+								magic_item: Item { name: "Empty".to_string(), effect: 0},
+								potion1: Item { name: "Empty".to_string(), effect: 0},
+								potion2: Item { name:"Empty".to_string(), effect: 0},
+								potion3: Item { name:"Empty".to_string(), effect: 0},
+							},
 		}
 	}
 }
 
+
 impl Player {
-	fn level_up(&self) {
+	fn level_up(&mut self) {
 		self.level += 1; 
-		self.damage += (stats_to_mods(self.stats.strength)) *  self.level;
-		self.speed += (stats_to_mods(self.stats.dex)) * self.level;
+		self.damage += (Stats::to_mods(self.stats.strength)) *  self.level;
+		self.speed += (Stats::to_mods(self.stats.dex)) * self.level;
 		self.armor_class += self.speed;
-		self.hit_points += (stats_to_mods(self.stats.con)) * self.level;
-		self.gold += (stats_to_mods(self.stats.cha) * self.level * 5) as i32;
+		self.hit_points += (Stats::to_mods(self.stats.con)) * self.level;
+		self.gold += (Stats::to_mods(self.stats.cha) * self.level * 5) as i32;
 	}
 }
 
-
+#[derive(Debug)]
 struct Stats {
 	strength: u8,
 	dex: u8,
@@ -144,61 +158,103 @@ impl Default for Stats {
 	
 
 impl Stats {
-	fn build_stats(&self) {
-		self.strength: roll_stat(),
-		self.dex: roll_stat(),
-		self.con: roll_stat(),
-		self.int: roll_stat(),
-		self.wis: roll_stat(),
-		self.cha: roll_stat(),
+
+	fn roll() -> u8 {
+		let r1 = rand::thread_rng().gen_range(1, 6);
+		let r2 = rand::thread_rng().gen_range(1, 6);
+		let r3 = rand::thread_rng().gen_range(1, 6);
+		let r4 = rand::thread_rng().gen_range(1, 6);
+	
+		let mut rolls = [r1, r2, r3, r4];
+	
+		rolls.sort_unstable();
+	
+		rolls[1] + rolls[2] + rolls[3]
+	}
+	
+	fn build(&mut self) {
+		self.strength = Stats::roll();
+		self.dex = Stats::roll();
+		self.con = Stats::roll();
+		self.int = Stats::roll();
+		self.wis = Stats::roll();
+		self.cha = Stats::roll();
+	}
+	
+	fn to_mods(stat: u8) -> i16 {
+	
+		let modifier = match stat {
+			3 | 4  => -3,
+			5 | 6  => -2,
+			7 | 8  => -1,
+			9 | 10 =>  0,
+			11 | 12 =>  1,
+			13 | 14 =>  2, 
+			15 | 16 =>  3,
+			17 | 18 =>  4,
+			19 | 20 =>  5,
+			_       =>  0,
+		};
+		
+		modifier
+	}
+	
+	fn print(&self) {
+	println!("STR: {}", self.strength);
+	println!("DEX: {}", self.dex);
+	println!("CON: {}", self.con);
+	println!("INT: {}", self.int);
+	println!("WIS: {}", self.wis);
+	println!("CHA: {}", self.cha);
 	}
 }
 
-	
+#[derive(Debug)]
 struct Inventory {
 	armor: Item,
 	weapon: Item,
 	magic_item: Item,
 	potion1: Item,
 	potion2: Item,
-	Potion3: Item
+	potion3: Item
 	}
 	
 impl Default for Inventory {
 	fn default() -> Inventory {
 		Inventory {
-			armor: Item { "Old Rags", 0 },
-			weapon: Item { "Bare Hands", 0},
-			magic_Item: Item { "Empty", 0},
-			potion1: Item {"Empty", 0},
-			potion2: Item {"Empty", 0},
-			potion3: Item {"Empty", 0},
-			}
+			armor: Item { name: "Old Rags".to_string(), effect: 0 },
+			weapon: Item { name: "Bare Hands".to_string(), effect: 0},
+			magic_item: Item { name: "Empty".to_string(), effect: 0},
+			potion1: Item { name: "Empty".to_string(), effect: 0},
+			potion2: Item { name:"Empty".to_string(), effect: 0},
+			potion3: Item { name:"Empty".to_string(), effect: 0},
 		}
 	}
 }
+
 			
 
 impl Inventory {
-	fn equip_armor(&self, player: &Player, item: &Item) {
+	fn equip_armor(&mut self, player: &mut Player, item: &Item) {
 		player.armor_class -= self.armor.effect;
-		self.armor.name = item.name;
+		self.armor.name = item.name.clone();
 		player.armor_class += item.effect;
 		}
 		
-	fn equip_weapon(&self, player: &Player, item: &Item) {
+	fn equip_weapon(&mut self, player: &mut Player, item: &Item) {
 		player.damage -= self.weapon.effect;
-		self.weapon.name = item.name;
+		self.weapon.name = item.name.clone();
 		player.damage += item.effect;
 		}
 		
-	fn equip_magic(&self, player: &Player, item: &Item) {
+	fn equip_magic(&mut self, player: &mut Player, item: &Item) {
 		player.magic -= self.magic_item.effect;
-		self.magic_item.name = item.name;
+		self.magic_item.name = item.name.clone();
 		player.magic += item.effect;
 		}
 	}
 
+#[derive(Debug)]
 struct Item {
 	name: String,
 	effect: i16
@@ -215,8 +271,9 @@ struct Creature {
 
 /*------------------Functions----------------*/
 
-fn loc_town(player: &mut Player) {
-}
+//TODO: Complete this function
+//fn loc_town(player: &mut Player) {
+//}
 
 fn print_line_break() {
 	println!(" ");
@@ -232,28 +289,6 @@ fn input_name() -> String {
 	name
 }
 
-fn roll_stat() -> u8 {
-	let r1 = rand::thread_rng().gen_range(1, 6);
-	let r2 = rand::thread_rng().gen_range(1, 6);
-	let r3 = rand::thread_rng().gen_range(1, 6);
-	let r4 = rand::thread_rng().gen_range(1, 6);
-	
-	let mut rolls = [r1, r2, r3, r4];
-	
-	rolls.sort_unstable();
-	
-	rolls[1] + rolls[2] + rolls[3]
-}
-
-
-fn print_stats(stats: &Stats) {
-	println!("STR: {}", stats.strength);
-	println!("DEX: {}", stats.dex);
-	println!("CON: {}", stats.con);
-	println!("INT: {}", stats.int);
-	println!("WIS: {}", stats.wis);
-	println!("CHA: {}", stats.cha);
-}
 	
 fn yes_or_no() -> bool {
 
@@ -264,23 +299,7 @@ fn yes_or_no() -> bool {
 		input.trim() == "Y" || input.trim() == "y" 			
 	}
 	
-fn stats_to_mods(stat: u8) -> i16 {
-	
-	let modifier = match stat {
-		 3 | 4  => -3,
-		 5 | 6  => -2,
-		 7 | 8  => -1,
-		 9 | 10 =>  0,
-		11 | 12 =>  1,
-		13 | 14 =>  2, 
-		15 | 16 =>  3,
-		17 | 18 =>  4,
-		19 | 20 =>  5,
-		_       =>  0,
-		};
-		
-		modifier
-}
+
 
 
 
